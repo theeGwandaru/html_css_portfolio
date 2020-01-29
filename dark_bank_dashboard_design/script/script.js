5b787d93e582b1b829d9b4dc68b20925b81cf603
theeGwandaru <muthawamunene@gmail.com>
2020-01-29 05:47:10 +0300

    init

---
+++ dark_bank_dashboard_design/script/script.js
@@ -0,0 +1,7 @@
+new Chartist.Line('#balance-cart-container', {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    series: [[12, 9, 7, 8, 5],]
+}, {
+    fullWidth: true,
+    chartPadding: {right: 0}
+});
